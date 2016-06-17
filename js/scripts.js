f122a757ef2f3931cf1f1d1b3790ee3dc5126d52
meaghanjones <meaghan.m.jones@gmail.com>
2016-06-17 15:03:37 -0700

    style html and add pictures to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 
 //backend
-function Pizza(toppings, size) {
+var Pizza = function (toppings, size, price) {
   this.toppings = toppings;
   this.pizzaSize = size;
+  this.price = price;
 }
 
 Pizza.prototype.price = function(){
@@ -11,40 +12,22 @@ Pizza.prototype.price = function(){
 
 
 //user interface logic
+
 $(document).ready(function() {
 
+  $("#price-calculate").click(function() {
+
+    var toppings = $("input:checkbox:checked").foreach(function(toppings){
+          var toppingsArray = [];
+          toppingsArray.push(toppings);
+        })
+        var size = $("#size").val();
+        var newPizza = new Pizza(toppings, size);
+        var pizzaPrice = newPizza.price();
 
 
+          $("#pizzapriceoutput").append("<p>" + pizzaPrice + "</p>");
+    });
 
-// $("#add-address").click(function() {
-  //   $("#new-addresses").append('<div class="new-address">' +
-  //                               '<div class="form-group">' +
-  //                               '<label for="new-street">Street</label>' +
-  //                               '<input type="text" class="form-control new-street">' +
-  //                                '</div>' +
-  //
-  //                                '<div class="form-group">' +
-  //                                '<label for="new-city">City</label>' +
-  //                                '<input type="text" class="form-control new-city">' +
-  //                                '</div>' +
-  //
-  //                                '<div class="form-group">' +
-  //                                '<label for="new-state">State</label>' +
-  //                                '<input type="text" class="form-control new-state">' +
-  //                                '</div>' +
-  //                                '</div>');
-  // });
-
-  $("form#price-calculate").submit(function() {
-    var inputtedTopping = $("input#new-first-name").val();
-    var inputtedPizzaSize = $("input#new-last-name").val();
-    var pizzaPrice = new Pizza(inputtedTopping, inputtedPizzaSize);
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
 
       });
-    });
