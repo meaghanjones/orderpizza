678f4b7bd96d0019ca8230f441c31b4115771d0a
meaghanjones <meaghan.m.jones@gmail.com>
2016-06-22 21:20:43 -0700

    fix indentation on js and html

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,16 @@ Pizza.prototype.price = function(){
   return this.pizzaSize;
 }
 
-
 //user interface logic
-
 $(document).ready(function() {
-
   $("#price-calculate").click(function() {
     var toppingsArray = [];
     var toppings = $("input:checkbox[name=type]:checked").each(function(){
-          toppingsArray.push($(this).val());
-        });
-
-    var size = $("input[name='pizzasizes']:checked").val();
-
-        var newPizza = new Pizza(size, toppings);
-        var pizzaPrice = newPizza.price();
-
-    $("#pizzapriceoutput").text("You owe: $" + pizzaPrice + ".");
+      toppingsArray.push($(this).val());
     });
-
-
-      });
+  var size = $("input[name='pizzasizes']:checked").val();
+  var newPizza = new Pizza(size, toppings);
+  var pizzaPrice = newPizza.price();
+  $("#pizzapriceoutput").text("You owe: $" + pizzaPrice + ".");
+  });
+});
